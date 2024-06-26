c8cfff5afa397ede47f7c2dc29b14e7dfc73ab1e
Coderganesh <gsb021@nist.edu>
2024-06-26 21:40:45 +0530

    Setup Frontend

---
+++ client/src/App.tsx
@@ -0,0 +1,33 @@
+import { useMemo } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
+import { Box, CssBaseline, ThemeProvider } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./scenes/navbar";
+import Dashboard from "@/scenes/dashBoard/index";
+import Prediction from "./scenes/prediction";
+import AddData from "./scenes/addData";
+function App() {
+  const theme = useMemo(() => createTheme(themeSettings), []);
+
+  return (
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Box height="100%" width="100%" padding="1rem 2rem 4rem 2rem">
+          <Navbar />
+          <Routes>
+            <Route path="/addData" element={<AddData />} />
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/prediction" element={<Prediction />} />
+          </Routes>
+        </Box>
+      </ThemeProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
